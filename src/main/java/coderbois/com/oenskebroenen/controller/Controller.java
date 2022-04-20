c98b69ab0c3c2b78bc359589f973a7ca09fd8a58
Lasse-R <lass531n@stud.kea.dk>
2022-04-20 12:46:45 +0200

    Controller test fuck mig

--- src/main/java/coderbois/com/oenskebroenen/controller/Controller.java
+++ src/main/java/coderbois/com/oenskebroenen/controller/Controller.java
@@ -2,20 +2,20 @@ package coderbois.com.oenskebroenen.controller;
 
 import coderbois.com.oenskebroenen.model.User;
 import coderbois.com.oenskebroenen.repository.LoadUsers;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import  org.springframework.stereotype.Controller;
 import java.util.ArrayList;
 
-@RestController
+@Controller
 public class Controller {
 
 
 
 
-    @GetMapping("/index")
-    public ArrayList<User> index(){
+    @GetMapping("/")
+    public String index(Model model){
         LoadUsers myUsers = new LoadUsers();
-        return myUsers.getAllUsers();
+        return "index";
     }
 }
