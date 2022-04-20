d107c09cd5f984dcdb31ef9c4414238c788fa99a
victor-dat21b <vict702k@stud.kea.dk>
2022-04-20 12:24:47 +0200

    Changes to system.properties and controller class

--- src/main/java/coderbois/com/oenskebroenen/controller/Controller.java
+++ src/main/java/coderbois/com/oenskebroenen/controller/Controller.java
@@ -1,15 +1,21 @@
 package coderbois.com.oenskebroenen.controller;
 
+import coderbois.com.oenskebroenen.model.User;
+import coderbois.com.oenskebroenen.repository.LoadUsers;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
+@RestController
 public class Controller {
 
 
 
 
     @GetMapping("/index")
-    public String index(){
-
-        return "index";
+    public ArrayList<User> index(){
+        LoadUsers myUsers = new LoadUsers();
+        return myUsers.getAllUsers();
     }
 }
