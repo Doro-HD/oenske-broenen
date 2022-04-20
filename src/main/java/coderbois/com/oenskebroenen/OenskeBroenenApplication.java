ede97926f1ad3b2fa86498231bb98b6ab1834948
DoroHD <david.bjakobsen@gmail.com>
2022-04-20 10:39:24 +0200

    initial commit

---
+++ src/main/java/coderbois/com/oenskebroenen/OenskeBroenenApplication.java
@@ -0,0 +1,13 @@
+package coderbois.com.oenskebroenen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OenskeBroenenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OenskeBroenenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coderbois/com/oenskebroenen/OenskeBroenenApplicationTests.java
@@ -0,0 +1,13 @@
+package coderbois.com.oenskebroenen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OenskeBroenenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
