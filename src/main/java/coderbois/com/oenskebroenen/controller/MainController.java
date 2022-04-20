ff4cc54ce79401b432b945a3602d72cd04968c2f
Lasse-R <lass531n@stud.kea.dk>
2022-04-20 13:05:55 +0200

    homepage + index testet

--- src/main/java/coderbois/com/oenskebroenen/controller/MainController.java
+++ src/main/java/coderbois/com/oenskebroenen/controller/MainController.java
@@ -1,19 +1,41 @@
 package coderbois.com.oenskebroenen.controller;
 
 import coderbois.com.oenskebroenen.model.User;
+import coderbois.com.oenskebroenen.repository.UserRepository;
 import coderbois.com.oenskebroenen.service.WishService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/index")
-    public ArrayList<User> index(){
+    @GetMapping("/")
+    public String index(Model model){
         WishService myWishService = new WishService();
 
-        return myWishService.getAllUsers();
+        model.addAttribute("user", new User());
+
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String indexPost(@ModelAttribute("user") User user, Model model){
+        WishService myWishService = new WishService();
+
+        System.out.println(user.getUsername() +" "+ user.getPassword());
+
+        return "homepage";
+    }
+
+    @GetMapping("/homepage")
+    public String homepage () {
+        return "homepage";
     }
 }
+
